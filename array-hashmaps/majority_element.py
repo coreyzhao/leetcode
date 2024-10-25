2356ca7f9e6e9815d5a99841b0584f0e2efe9fb3
Corey Zhao <coreyzh2004@gmail.com>
2024-10-25 09:57:28 -0400

    majele

---
+++ array-hashmaps/majority_element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        seen = {}
+
+        for i in nums:
+            if i in seen:
+                seen[i] += 1
+            else:
+                seen[i] = 1
+
+        for i in seen:
+            if seen[i] > len(nums)/2:
+                return i
