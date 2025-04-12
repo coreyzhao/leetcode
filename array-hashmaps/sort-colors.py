f3c9381e7d67a7460badee90b7b8c2fa380399a1
Corey Zhao <coreyzh2004@gmail.com>
2025-04-12 10:41:27 -0400

    daily

---
+++ array-hashmaps/sort-colors.py
@@ -0,0 +1,31 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        counts = [0,0,0]
+        for i in nums:
+            counts[i] += 1
+
+        red = counts[0]
+        white = counts[1]
+        blue = counts[2]
+
+        print(red, white, blue)
+
+
+        for i in range(len(nums)):
+            if red > 0:
+                nums[i] = 0
+                red -= 1
+                continue
+            if white > 0:
+                nums[i] = 1
+                white -= 1
+                continue
+            if blue > 0:
+                nums[i] = 2
+                blue -= 1
+                continue
+
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
