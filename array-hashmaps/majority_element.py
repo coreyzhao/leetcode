633bf9542e73cf4c6611143bb7cffe3d25673a2b
coreyzhao <coreyzh2004@gmail.com>
2025-04-27 20:54:42 -0400

    d

--- array-hashmaps/majority_element.py
+++ array-hashmaps/majority_element.py
@@ -12,3 +12,25 @@ class Solution:
         for i in seen:
             if seen[i] > len(nums)/2:
                 return i
+            
+#att 2
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        element_map = {}
+
+        for i in nums:
+            if i in element_map:
+                element_map[i] += 1
+            else:
+                element_map[i] = 1
+        max_count = -9999
+        max_num = 0
+
+        for i in element_map:
+          
+            if element_map[i] > max_count:
+                max_count = element_map[i]
+                max_num = i
+
+        return max_num
