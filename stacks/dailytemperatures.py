cb739bf82ef0aaf7c7ef8220b4473caa04313325
coreyzhao <coreyzh2004@gmail.com>
2025-04-29 11:28:27 -0400

    d

--- stacks/dailytemperatures.py
+++ stacks/dailytemperatures.py
@@ -11,3 +11,23 @@ class Solution:
             stack.append([t,i])
 
         return res
+    
+
+#2
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+        res = [0] * len(temperatures)
+
+        stack = []
+
+        for i, temp in enumerate(temperatures):
+
+            while stack and temp > stack[-1][0]:
+                info_ls = stack.pop()
+                res[info_ls[1]] = i - info_ls[1]
+            
+            stack.append([temp, i])
+
+        return res
