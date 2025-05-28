f84c7ee4390520dde068653eb1a5e5ce9f52ceed
coreyzhao <coreyzh2004@gmail.com>
2025-05-28 19:07:11 -0400

    d

--- sliding-windows/find_index_of_first_occur_In_str.py
+++ sliding-windows/find_index_of_first_occur_In_str.py
@@ -8,3 +8,15 @@ class Solution:
 
         return -1
 
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        p = 0
+
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+
+
+
+        return -1
+
