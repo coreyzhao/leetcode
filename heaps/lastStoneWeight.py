151b1b7d6841932128c94c6478034344c19ecdc4
coreyzhao <coreyzh2004@gmail.com>
2025-06-01 18:57:06 -0400

    daily

--- heaps/lastStoneWeight.py
+++ heaps/lastStoneWeight.py
@@ -18,4 +18,29 @@ class Solution:
             return 0
 
         return heapq._heappop_max(stones)
-            
+
+#
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        for i in range(len(stones)):
+            stones[i] = stones[i] * -1
+
+        heapq.heapify(stones)
+
+        while (len(stones) > 1):
+            largest = -heapq.heappop(stones)
+            next_largest = -heapq.heappop(stones)
+
+            new_stone = largest - next_largest
+            if (new_stone):
+                heapq.heappush(stones, -1 * new_stone)
+
+        if len(stones) == 1:
+            return stones[0] *-1
+
+        return 0
+
+
+        
