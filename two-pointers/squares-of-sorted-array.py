d94a4d5161784d01f670c54d9aac349a84325fd4
coreyzhao <coreyzh2004@gmail.com>
2025-06-02 21:31:32 -0400

    d

--- two-pointers/squares-of-sorted-array.py
+++ two-pointers/squares-of-sorted-array.py
@@ -15,3 +15,20 @@ class Solution:
                 r -= 1
 
         return res
+    
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        l = 0
+
+        r = len(nums) - 1
+        res = []
+
+        while l <= r:
+            if abs(nums[l]) > abs(nums[r]):
+                res.insert(0, nums[l] ** 2)
+                l += 1
+            else:
+                res.insert(0, nums[r] ** 2)
+                r -= 1
+
+        return res
