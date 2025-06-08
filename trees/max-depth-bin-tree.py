8d40c18bebe0c83b4f4e7e1cfea3cf3d9d8dccc7
Corey Zhao <coreyzh2004@gmail.com>
2025-06-07 22:10:15 -0400

    daily

--- trees/max-depth-bin-tree.py
+++ trees/max-depth-bin-tree.py
@@ -10,3 +10,32 @@ class Solution:
             return 0
         
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right)) 
+    
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        def maxDep(root, count, cur):
+            
+            if not root:
+                cur = max(count, cur)
+                return cur
+
+            count += 1
+            cur_max = max(maxDep(root.left, count, cur), maxDep(root.right, count, cur))
+            count -= 1
+
+            return cur_max
+
+        return maxDep(root, 0, 0)
+
+            
+
+            
