e58671384e7b2b227af99c575d9b47fc0722cc25
Corey Zhao <coreyzh2004@gmail.com>
2025-06-08 22:10:07 -0400

    daily

--- trees/invert-bin-tree.py
+++ trees/invert-bin-tree.py
@@ -13,3 +13,27 @@ class Solution:
         temp.left = self.invertTree(root.right)
         temp.right = self.invertTree(root.left)
         return temp
+    
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        def invTree(root):
+            if not root:
+                return
+
+            og_left = root.left
+            root.left = root.right
+            root.right = og_left
+            
+            invTree(root.left)
+            invTree(root.right)
+
+        invTree(root)
+        return root
