77892469ba807d435e5ded4126c70d1ab373497f
Corey Zhao <coreyzh2004@gmail.com>
2025-06-20 11:40:01 -0400

    d

--- two-pointers/stock.py
+++ two-pointers/stock.py
@@ -19,3 +19,21 @@ class Solution:
 
         return profitmax
 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l = 0
+        r = 1
+        maxProfit = 0
+
+        while r < len(prices):
+            if prices[l] > prices[r]:
+                l = r
+                r = l + 1
+            else:
+                cur = prices[r] - prices[l]
+                maxProfit = max(maxProfit, cur)
+
+                r += 1
+
+        return maxProfit
+            
