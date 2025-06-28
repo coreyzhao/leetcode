20b9cd30b4c92240f8343dfdc7d072f4a501074f
Corey Zhao <coreyzh2004@gmail.com>
2025-06-27 21:23:34 -0400

    d

--- dynamic-programming/climbing-stairs.py
+++ dynamic-programming/climbing-stairs.py
@@ -9,3 +9,20 @@ class Solution:
         for i in range(2, n+1):
             dp[i] = dp[i-1] + dp[i-2]
         return dp[n]
+    
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        last = 1
+        pre_last = 1
+
+
+        for i in range(n - 1):
+            temp = pre_last
+            pre_last = pre_last + last
+
+            last = temp
+
+        return pre_last
+    
+    
