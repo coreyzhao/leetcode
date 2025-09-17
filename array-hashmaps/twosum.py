d4ec9592c31b7f2f9d93e92503610a724fca3bdf
Corey Zhao <coreyzh2004@gmail.com>
2025-09-16 20:34:17 -0400

    2sum

--- array-hashmaps/twosum.py
+++ array-hashmaps/twosum.py
@@ -12,4 +12,19 @@ class Solution:
 
 
 
-        
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        my_dict = {} # val : index
+        i1 = 0
+        i2 = 0
+
+        for i in range(len(nums)):
+            if (target - nums[i]) in my_dict:
+                i1 = my_dict[target - nums[i]]
+                i2 = i
+
+            my_dict[nums[i]] = i
+
+        return [i1, i2]
+
+                
