32f216799a6bd1cc8e2ab63009ec76067258865f
Corey Zhao <coreyzh2004@gmail.com>
2025-09-17 21:09:43 -0400

    koko

--- binary search/koko-eating-bananas.py
+++ binary search/koko-eating-bananas.py
@@ -23,3 +23,27 @@ class Solution:
 
 
         return min_speed
+    
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l = 1
+        r = max(piles)
+        res = r
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            cur_h = 0
+            for i in piles:
+                cur_h += math.ceil(i / mid)
+
+            if cur_h > h:
+                l = mid + 1
+
+            else:
+                res = min(mid, r)
+                r = mid - 1
+
+        return res
+
