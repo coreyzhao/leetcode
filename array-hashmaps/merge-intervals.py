d0229bab655711b68a5cdebc50572097891f8d9d
Corey Zhao <coreyzh2004@gmail.com>
2025-09-22 14:50:29 -0400

    d

--- array-hashmaps/merge-intervals.py
+++ array-hashmaps/merge-intervals.py
@@ -19,3 +19,25 @@ class Solution:
 
         return res
 
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda ls: ls[0])
+
+        if len(intervals) == 1:
+            return intervals
+        print(intervals)
+        prev = intervals[0]
+        res = []
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= prev[1]:
+                prev[1] = max(prev[1], intervals[i][1])
+
+            else:
+                res.append(prev)
+                prev = intervals[i]
+
+        res.append(prev)
+        return res
+
+
