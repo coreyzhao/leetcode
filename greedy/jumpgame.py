0fa5a3e31e0a118ea201f7a06a41d3f2bbde71ae
Corey Zhao <coreyzh2004@gmail.com>
2025-09-26 23:08:48 -0400

    greedy

--- greedy/jumpgame.py
+++ greedy/jumpgame.py
@@ -11,3 +11,15 @@ class Solution:
             return True
         else:
             return False
+        
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+
+        for i in range(len(nums) -2,-1,-1):
+            if i + nums[i] >= goal:
+                print(goal)
+                goal = i
+
+        return True if goal == 0 else False
