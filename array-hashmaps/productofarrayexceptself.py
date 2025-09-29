1859078ab194295933e6e6083d61ab5931f94f59
Corey Zhao <coreyzh2004@gmail.com>
2025-09-28 21:05:16 -0400

    prefixsum

---
+++ array-hashmaps/productofarrayexceptself.py
@@ -0,0 +1,16 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        res = [1] * len(nums)
+
+        pre = 1
+        for i in range(len(nums)):
+            res[i] = pre
+            pre = pre * nums[i]
+
+        suf = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] = res[i] * suf
+            suf = suf * nums[i]
+            
+        return res
