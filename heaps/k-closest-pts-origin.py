b4df9ff1a05951edc47bea0d58d24f09914234cf
Corey Zhao <coreyzh2004@gmail.com>
2025-10-02 08:14:30 -0400

    heapq pq

--- heaps/k-closest-pts-origin.py
+++ heaps/k-closest-pts-origin.py
@@ -14,3 +14,20 @@ class Solution:
             k -= 1
 
         return res
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+        for i in range(len(points)):
+            dist = (points[i][0]**2) + (points[i][1]**2)
+            points[i].insert(0, dist)
+
+        heapq.heapify(points)
+        res = []
+
+        for i in range(k):
+            ls = heapq.heappop(points)
+
+            res.append([ls[1], ls[2]])
+
+        return res
