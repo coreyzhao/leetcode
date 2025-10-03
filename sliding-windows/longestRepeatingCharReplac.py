088804659fb7dbb4d8258aec6bd8488499155166
Corey Zhao <coreyzh2004@gmail.com>
2025-10-02 20:50:43 -0400

    slidingwindows

--- sliding-windows/longestRepeatingCharReplac.py
+++ sliding-windows/longestRepeatingCharReplac.py
@@ -23,3 +23,27 @@ class Solution:
             max_len = max(max_len, r - l + 1)
 
         return max_len
+    
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        my_map = {}
+        maxlen = 0
+        maxfreq = 0
+        l = 0
+        for r in range(len(s)):
+            windowsize = r - l + 1
+
+            my_map[s[r]] = my_map.get(s[r], 0) + 1
+            maxfreq = max(maxfreq, my_map[s[r]])
+
+            while windowsize - maxfreq > k:
+                my_map[s[l]] -= 1
+                l += 1
+                windowsize = r - l + 1
+
+            maxlen = max(windowsize, maxlen)
+
+        
+
+        return maxlen   
