dc9ec017df2c45847ae27cfbb793326ff470e923
Corey Zhao <coreyzh2004@gmail.com>
2025-10-03 21:14:51 -0400

    hashmap

--- array-hashmaps/group_anagrams.py
+++ array-hashmaps/group_anagrams.py
@@ -15,3 +15,30 @@ class Solution:
             final_ls.append(hash_mp[key])
 
         return final_ls
+    
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        my_map = {}
+        
+        
+        for i in range(len(strs)):
+            
+            count = [0] * 26
+            for c in strs[i]:
+                index = ord(c) - ord("a")
+                count[index] += 1
+
+            if tuple(count) in my_map:
+                my_map[tuple(count)] = my_map[tuple(count)] + [strs[i]]
+            else:
+                my_map[tuple(count)] = [strs[i]]
+
+        res = []
+        for key in my_map:
+            res.append(my_map[key])
+
+        return res
+
+
+        print(my_map)
+
