2e7383ccf7542e3ce1bdac76e1d562c31fa2d724
Corey Zhao <coreyzh2004@gmail.com>
2025-10-05 15:28:01 -0400

    binary search tree

--- trees/lowest-com-ancestor-BST.py
+++ trees/lowest-com-ancestor-BST.py
@@ -17,3 +17,34 @@ class Solution:
                 root = root.left
             else:
                 root = root.right
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        if p.val > q.val:
+            temp = p
+            p = q
+            q = temp
+
+        def findLCA(root, p, q):
+            print(p.val, q.val)
+            if p.val <= root.val and q.val >= root.val:
+                return root
+
+            else:
+                if p.val < root.val and q.val < root.val:
+                    return findLCA(root.left, p, q)
+                else:
+                    return findLCA(root.right, p , q)
+
+        return findLCA(root, p, q)
+
+        
